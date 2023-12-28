e0f51ab61d77450aee6eec6b294869154fbfa64e
Thomas Hodges <thodges@javascriptonline.org>
2023-12-27 21:17:51 -0800

    start + vars

---
+++ cmd/tutorial_1/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main(){
+	var intNum int16 = 32767
+	intNum++
+	fmt.Println(intNum)
+
+	var floatNum float32 = 12345678.9
+	fmt.Println(floatNum)
+
+	var floatNum32 float32 = 10.1
+	var intNum32 int32 = 2
+
+	var result float32 = floatNum32 / float32(intNum32)
+	fmt.Println(result)
+
+	var intnum1 int = 3
+	var intnum2 int = 2
+	fmt.Println(intnum1 / intnum2)
+	fmt.Println(intnum1 % intnum2)
+
+	var myString string = "Hello \nWorld"
+	fmt.Println(myString)
+	fmt.Println(len(myString))
+	fmt.Println(utf8.RuneCountInString(myString))
+
+	var myBoolean bool
+	fmt.Println(myBoolean)
+
+	myvar := "text"
+	fmt.Println(myvar)
+
+	var1, var2 := 10, 20
+	fmt.Println(var1, var2)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thodges314/go_tutorials.git
+
+go 1.21.5
