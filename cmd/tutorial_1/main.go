33abbc1fa9ccaa3196012d0a249fca43fd3ef637
Thomas Hodges <thodges@javascriptonline.org>
2023-12-27 22:04:15 -0800

    loops

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -21,4 +21,46 @@ func main(){
 	var intSlice3 []int32 = make([]int32, 3, 8) // 3 is length, 8 is capacity
 	fmt.Printf("The length is %v with capaticy %v.\n",len(intSlice3),cap(intSlice3))
 	fmt.Println(intSlice3)
+
+	var myMap map[string]uint8 = make(map[string]uint8) // keys are string, values are unsigned 8
+	fmt.Println(myMap)
+
+	var myMap2 = map[string]uint8{"Adam":32,"Sarah":45}
+	fmt.Println(myMap2["Adam"])
+	fmt.Println(myMap2["Sarah"])
+	// fmt.Println(myMap["jason"])
+	var age, ok = myMap2["Jason"]
+	// delete(myMap2, "Adam")
+	if ok {
+    fmt.Printf("Tha age is %v\n", age)
+  } else {
+    fmt.Println("Invalid Name!!!!")
+  }
+
+	for name := range myMap2 { // order is not guaranteed
+		fmt.Printf("Name: %v\n",name)
+	}
+
+	for i,v := range intArr { // index, value
+		fmt.Printf("Index: %v, Value: %v\n",i,v)
+	}
+
+	var i int = 0
+	for i <10{
+		fmt.Println(i)
+    i++
+	}
+
+	i = 0
+	for{
+		if i >= 10{
+			break
+		}
+		fmt.Println(i)
+    i++
+	}
+
+	for j:=0; j<10; j++ {
+		fmt.Println((j))
+	}
 }
