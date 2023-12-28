2997fe6d2ea152b47e79dd48c7f69d16cb4df321
Thomas Hodges <thodges@javascriptonline.org>
2023-12-27 21:45:00 -0800

    other way to define array

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -3,12 +3,6 @@ package main
 import "fmt"
 
 func main(){
-	var intArr [3]int32 
-	intArr[1] = 123
-	fmt.Println(intArr[0])
-	fmt.Println(intArr[1:3])
-
-	fmt.Println(&intArr[0])
-	fmt.Println(&intArr[1])
-	fmt.Println(&intArr[2])
+	intArr := [...]int32{1,2,3}
+	fmt.Println(intArr)
 }
