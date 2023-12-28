f55da2e3bbe96831a794520972c654b1fd1427a5
Thomas Hodges <thodges@javascriptonline.org>
2023-12-27 21:38:44 -0800

    functions and if/then/else + switch

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,46 +1,52 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"unicode/utf8"
 )
 
 func main(){
-	var intNum int16 = 32767
-	intNum++
-	fmt.Println(intNum)
+	// var printValue string = "Hello, World!"
+	// printMe(printValue)
+	var num int = 11
+	var denom int = 4
+	var result, remainder, err = intDivision(num, denom)
+	// if(err!= nil){
+	// 	fmt.Printf(err.Error())
+	// } else if remainder == 0{
+	// 	fmt.Printf("The result of integer division is %v.", result)
+	// } else{
+	// 	fmt.Printf("The result of integer division is %v and the remainder is %v.", result, remainder)
+	// }
+	switch{
+		case err!= nil:
+      fmt.Printf(err.Error())
+    case remainder == 0:
+      fmt.Printf("The result of integer division is %v.", result)
+    default:
+      fmt.Printf("The result of integer division is %v and the remainder is %v.", result, remainder)
+	}
+	// in switch, break is implied
+	switch remainder{
+	case 0:
+		fmt.Printf("The division was exact.")
+	case 1,2:
+		fmt.Printf("The division was close.")
+	default:
+		fmt.Printf("The division was not exact.")
+	} 
+}
 
-	var floatNum float32 = 12345678.9
-	fmt.Println(floatNum)
+func printMe(printValue string){
+	fmt.Println(printValue)
+}
 
-	var floatNum32 float32 = 10.1
-	var intNum32 int32 = 2
-
-	var result float32 = floatNum32 / float32(intNum32)
-	fmt.Println(result)
-
-	var intnum1 int = 3
-	var intnum2 int = 2
-	fmt.Println(intnum1 / intnum2)
-	fmt.Println(intnum1 % intnum2)
-
-	var myString string = "Hello \nWorld"
-	fmt.Println(myString)
-	fmt.Println(len(myString))
-	fmt.Println(utf8.RuneCountInString(myString))
-
-	var myBoolean bool
-	fmt.Println(myBoolean)
-
-	myvar := "text"
-	fmt.Println(myvar)
-
-	var1, var2 := 10, 20
-	fmt.Println(var1, var2)
-
-	const myConst string = "Hello World"
-	fmt.Println(myConst)
-
-	const pi float32 = 3.14159
-	fmt.Println(pi)
+func intDivision(num int, denom int) (int, int, error){
+	var err error
+	if denom == 0{
+    err = errors.New("Denominator cannot be 0")
+  } 
+	var result int = num / denom
+	var remainder int = num % denom
+	return result, remainder, err
 }
